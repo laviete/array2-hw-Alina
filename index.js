47f6d74c2c5ee08bd0d4a4f8be1ca416649b4cd0
Alina <135998117+laviete@users.noreply.github.com>
2023-07-24 23:22:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,19 @@
-const array = [1, 2, 3, 1, 5, 6, 1, 1, 2, 5];
+const array = [1, 2, 3, 1, 5, 6, 1, 1, 2, 5]; 
 const result = array.filter(
   (elem, index) =>
     array.indexOf(elem) !== index && 
     array.lastIndexOf(elem) === index
 );
-console.log(result);
+console.log(result); // [1, 2, 5]
 
-function getDuplicateNums(arr) {
+function getDuplicatesNums(arr) {
   return arr.filter(
     (elem, index) =>
     arr.indexOf(elem) !== index && 
     arr.lastIndexOf(elem) === index
   );
 }
-const arr = [1, 2, 3, 1, 5, 6, 1, 2, 5];
-console.log(getDuplicatesNums(arr));
+const arr = [1, 2, 3, 4, 4, 5, 5, 6, 6];
+const arr1 = [1, 2, 3];
+console.log(getDuplicatesNums(arr)); // [4, 5, 6]
+console.log(getDuplicatesNums(arr1)); //[]
